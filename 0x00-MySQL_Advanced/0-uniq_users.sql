60307fad7b4fdfaf4612431870a8278cb0674bd5
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-08-08 08:14:55 +0300

    task-0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- Task 0: Create a users table with unique email
+-- Table creation with unique email constraint
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
