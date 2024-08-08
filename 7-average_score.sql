edd3c7c1705b715a7b07bff3d1dcb02f111de533
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-08-08 23:26:01 +0300

    task-7.1

--- 7-average_score.sql
+++ 7-average_score.sql
@@ -1,9 +1,10 @@
 -- Task 7: Average score
 -- This script creates a stored procedure ComputeAverageScoreForUser that computes and stores the average score for a student.
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER *
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     DECLARE avg_score FLOAT;
-    SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
+    SELECT AVG(score) INTO avg_score FROM corrections WHERE corrections.user_id = user_id;
     UPDATE users SET average_score = avg_score WHERE id = user_id;
 END*
