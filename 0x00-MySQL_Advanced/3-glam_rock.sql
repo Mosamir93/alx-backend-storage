1c305ba743de83e6d77ec5339997fc01b0254594
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-08-08 08:53:05 +0300

    task-3.2

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,5 +2,5 @@
 -- Query to list Glam rock bands by longevity
 SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
 FROM metal_bands
-WHERE main_style = 'Glam rock'
+WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
 ORDER BY lifespan DESC;
