b763340270b24985f26623c96218e27d0e0dca86
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-10-22 23:44:31 +0300

    task-8.1

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,8 +2,8 @@
 """
 A Python function that lists all documents in a collection
 """
-from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
+    """Lists all documents in a collection."""
     return list(mongo_collection.find())
