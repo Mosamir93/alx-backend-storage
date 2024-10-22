bb9d6d1d5fd2446fe953602b2f60ab9da74c94a1
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-10-23 00:11:03 +0300

    task-10.1

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -9,4 +9,4 @@ def update_topics(mongo_collection, name, topics):
     """
     Updates all topics of a school document.
     """
-    mongo_collection.update_one({'name': name}, {"$set": {'topics': topics}})
+    mongo_collection.update_many({'name': name}, {"$set": {'topics': topics}})
