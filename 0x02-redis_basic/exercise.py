66a8a7ed3ab6da2eb1a4f1812d8e9c8750f75f01
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-10-24 02:33:47 +0300

    task-1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """A Cache class."""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -16,3 +16,19 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None):
+        """Takes a key string argument and an
+        optional Callable argument named fn"""
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """Retrieve and convert the data to a string."""
+        return self.get(key).decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        """Retrieve and convert the data to an integer."""
+        return int(self.get(key))
