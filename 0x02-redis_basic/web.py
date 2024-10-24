6f578964205b7072bbcfacfe1704f28d1822efcd
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-10-24 08:46:04 +0300

    final

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,13 +8,14 @@ def get_page(url: str) -> str:
     """Track how many times a particular URL was accessed in the key
     "count:{url}" and cache the result with an expiration time of 10 seconds.
     """
-    count = f"count:{url}"
+    count_key = f"count:{url}"
     r = redis.Redis()
-    r.incr(count)
+    r.incr(count_key)
 
     cached_page = r.get(url)
     if cached_page:
         return cached_page.decode('utf-8')
     response = requests.get(url)
     r.setex(url, 10, response.content)
-    return response.content
+    return response.content.decode('utf-8')
+    
