77bb286ce7aca71a8ae681ce8bd2cd437cd5b008
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-10-24 08:49:56 +0300

    final2

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -14,8 +14,7 @@ def get_page(url: str) -> str:
 
     cached_page = r.get(url)
     if cached_page:
-        return cached_page.decode('utf-8')
+        return cached_page
     response = requests.get(url)
     r.setex(url, 10, response.content)
-    return response.content.decode('utf-8')
-    
+    return response.content
