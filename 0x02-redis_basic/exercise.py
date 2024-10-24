105c0ae8b7372f3915b0f5a4bc2c99243be53dc6
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-10-24 04:04:00 +0300

    task-4.final4

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -34,12 +34,9 @@ def call_history(method: Callable) -> Callable:
 
 def replay(fn: Callable) -> None:
     """ Display the history of calls of a particular function."""
-    inputs_key = fn.__qualname__ + ":inputs"
-    outputs_key = fn.__qualname__ + ":outputs"
-
     r = redis.Redis()
-    inputs = r.lrange(inputs_key, 0, -1)
-    outputs = r.lrange(outputs_key, 0, -1)
+    inputs = r.lrange(fn.__qualname__ + ":inputs", 0, -1)
+    outputs = r.lrange(fn.__qualname__ + ":outputs", 0, -1)
     print(f"{fn.__qualname__} was called {len(outputs)} times:")
     for inp, out in zip(inputs, outputs):
         print(f"{fn.__qualname__}(*{(inp.decode('utf-8'))}) -> "
